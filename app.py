cd3a5d548aaaeebc3389a6db809dba5f87b35850
Rahul Babu K <rahulbabu101@gmail.com>
2020-05-15 23:36:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,6 @@ import re
 import pandas as pd
 import numpy as np
 from googlesearch import search 
-
-
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from sklearn.cluster import KMeans 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import normalize
-from keras.models import Sequential
-from keras.layers import Dense, Embedding, LSTM
-from keras.optimizers import Adam
-from keras import models as models
 import requests
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -26,135 +14,268 @@ questiondf = pd.read_csv("question.csv")
 asindf = pd.read_csv("asindf.csv")
 answerdf = pd.read_csv("answer.csv")
 
-
-
-
-
+with open('asindftfidfvector.pkl', 'rb') as f:
+    asintfidf_vectorizer = pickle.load(f)
+with open('asindftfidf.pkl', 'rb') as f:
+    asintfidf_matrix = pickle.load(f)
+with open('qtfidfvector.pkl', 'rb') as f:
+    qtfidf_vectorizer = pickle.load(f)
+with open('questiontfidf.pkl', 'rb') as f:
+    qtfidf_matrix = pickle.load(f)
 
 def clean(x):
-    x = re.sub(r'http\S+', '', x)
-    x = re.sub(r'[^\w]', ' ', x).lower()
-    x = re.sub(r'(?:^| )\w(?:$| )', ' ', x).strip()
-    x = x.replace('  ',' ')
-
+    x = x.lower()
+    x = re.sub(r'[^\w]', ' ', x)
     return x
+def productq(inputq):
+    j = 'product not found'
+    query = "amazon "+inputq
+    try :
+        for j in search(query, tld="com", num=1, stop=1):
+            j = j.split(sep='.com/')[1]
+            j = j.split(sep='/')[0]
+    except :
+        j = 'product not found'
+    return j
+#Greeting Inputs
+import random
+GREETING_INPUTS = ["hi", "hello", "greetings", "wassup", "hey"]
+GREETING_RESPONSES=["Hey", "hi",  "what's good", "hello", "hey there"]
+def greeting(sentence):
+  for word in sentence.split():
+    if word.lower() in GREETING_INPUTS:
+      return random.choice(GREETING_RESPONSES)
+
+def cosine(question) :
+    result0 = []
+    result1 = []
+    result2 = []
+    query_vect = qtfidf_vectorizer.transform([question])
+    similarity = cosine_similarity(query_vect, qtfidf_matrix)
+    top_5_simmi = similarity[0].argsort()[-15:][::-1]
+
+    for i in top_5_simmi:
+        result0.append(questiondf.iloc[i]['question'])
+        result1.append(answerdf.iloc[i]['answer'])       
+        result2.append(similarity[0, i])
+
+    return result0 , result1 , result2
+
+def asin(question) :
+    query_vect = asintfidf_vectorizer.transform([question])
+    similarity = cosine_similarity(query_vect, asintfidf_matrix)
+    top_5_simmi = similarity[0].argsort()[-5:][::-1]
+    result = []
+    result1 = []
+    for i in top_5_simmi:
+        result.append(asindf.iloc[i]['asin'])  
+        result1.append(similarity[0, i])  
+    return result , result1
+
+
+def response(user_response , firstq , typeq , variable):
+    user_response = clean(user_response)
+    robo_response = ''
+    score = [0]
+    action = 0
+    try :
+        if (typeq == 0) : #first robo response
+            firstq = user_response
+            question , answer , score = cosine(user_response)
+            flag = 1
+            if ((score[0] > 0.9) and (typeq == 0)): #lucky
+                robo_response = robo_response + answer[0]
+                flag = 8
+                variable = question
+            elif((score[0] <= 0.9) and (typeq== 0)):
+                productcode , similarity = asin(user_response)
+                if similarity[0]>0.4:
+                    robo_response = robo_response+"I am confused with the product you are talking about. I am searching it in google please wait"
+                    flag = 33
+                    variable = productcode[0] + user_response
+                    action = 1
+                elif (similarity[0]<= 0.4):
+                    robo_response = robo_response+"I am confused with the product you are talking about. I am searching it in google please wait"
+                    flag = 34
+                    variable = productcode
+                    action = 1
+        elif(typeq == 33):
+            productname = productq(variable)
+            if productname != 'product not found':
+                robo_response = robo_response+"Is it "+productname + " ?"
+                flag = 2
+                variable = productcode
+            else :
+                robo_response = robo_response+"I am finding similar questions for you"
+                flag = 3
+                variable = productcode
+
+        elif(typeq == 34):
+            num = 0
+            productcode = variable
+            for i in productcode:
+                num += 1
+                k = i
+                if  productq(i) != 'product not found':
+                    k = productq(i)
+                robo_response = robo_response + "<br> " + str(num) + k
+            flag = 4
+            variable = productcode
+            robo_response = robo_response+"<br> Reply product number if your product is in it"
+
+
+        elif typeq == 1 : #flag1 lucky answers
+            robo_response = variable[int(float(user_response))]
+        elif typeq == 2 : #flag2 yes/no
+            if "yes" in user_response:
+                question , answer , score = cosine(firstq + variable[0] )
+                robo_response = robo_response+"Here I found top questions and answers related to your query <br> 1."+question[0] +"<br> 2."+question[1] +"<br> 3."+question[2] +"<br> 4."+question[3] +"<br> 5."+question[4]
+                flag = 6
+                variable = answer
+            elif "no" in user_response:
+                productcode = variable
+                robo_response = robo_response+"I am confused with the product you are talking about i found similar product based on your question reply me its index"
+                num = 0
+                for i in productcode:
+                    num += 1
+                    k = i
+                    if  productq(i) != 'product not found':
+                        k = productq(i)
+                    robo_response = robo_response + "<br>" + str(num) + k
+                robo_response = robo_response+"<br> Reply product number if your product is in it"
+                flag = 4
+                variable = productcode
+        
+
+
+        elif typeq == 3 :  #flag 3
+            question , answer , score = cosine(variable)
+            robo_response = robo_response+"Here I found top questions and answers related to your query <br> 1."+question[0] +"<br> 2."+question[1] +"<br> 3."+question[2] +"<br> 4."+question[3] +"<br> 5."+question[4]
+            flag = 6
+            variable = answer
+        elif typeq == 4 : #choose product
+            try:
+                if 0 < (int(float(user_response)))< 6 :
+                    k = variable[int(float(user_response))-1]
+                    question , answer , score = cosine(firstq + k)
+                    robo_response = robo_response+"Here I found top questions and answers related to your query <br> 1."+question[0] +"<br> 2."+question[1] +"<br> 3."+question[2] +"<br> 4."+question[3] +"<br> 5."+question[4]
+                    flag = 6
+                    variable = answer
+                elif "not" in user_response :
+                    robo_response = "Please tell me more about it"
+                    flag = 0
+                elif "no" in user_response :
+                    robo_response = "Please tell me more about it"
+                    flag = 0
+                else :
+                    robo_response = "Invalid input Do you want to search other question"
+                    flag = 7
+                    variable = 0
+            except :
+                if "not" in user_response :
+                    robo_response = "Please tell me more about it"
+                    flag = 0
+                elif "no" in user_response :
+                    robo_response = "Please tell me more about it"
+                    flag = 0
+                else :
+                    robo_response = "Invalid input Do you want to search other question"
+                    flag = 7
+                    variable = 0
+            
+        elif typeq == 6 : #answers
+            try :
+                if 0 < (int(float(user_response)))< 6 :
+                    robo_response = variable[int(float(user_response))-1]
+                    robo_response = robo_response+"If you want another questions answer , tell me its number"
+                    flag = 6
+                    
+                
+                elif "no" in user_response :
+                    robo_response = "Thank you for your query"
+                    flag = 0
+                    variable = 0
+                    firstq = 0
+                else :
+                    robo_response = "Invalid input Do you want to search other question"
+                    flag = 7
+                    variable = 0
+            except :
+                robo_response = "Invalid input Do you want to search other question"
+                flag = 7
+                variable = 0
+            
+        elif typeq == 7: # reset
+            if "yes" in user_response:
+                flag = 0
+                variable = 0
+                firstq = 0
+                robo_response = "Okay ... tell me your next question"
+            elif "no" in user_response:
+                flag =9
+                robo_response = "Thank you"
+
+        elif typeq == 8 :#not lucky
+            if "no" in user_response:
+                flag = 101
+            else :
+                flag = 7
+        elif typeq == 9 or user_response == 'nextquestion' or user_response == 'stop' :#thankyou
+            robo_response = "you can tell me another question"
+            flag = 0
+            variable = 0
+            firstq = 0
+    except :
+            robo_response = "Invalid input tell me another question"
+            flag = 0
+            variable = 0
+            firstq = 0
+
+    return robo_response , flag , variable ,firstq ,action
+
+
+    
+typeq = 0
+firstq = 0
+variable = 0
+resp = 0
+
+def robot(user_response):
+    global resp 
+    global typeq 
+    global variable 
+    global firstq
+    user_response = clean(user_response)
+    if(user_response != 'bye'):
+      if('thanks' in user_response  or 'thank' in user_response or 'thankyou'in user_response ):
+        k ="DOCBot: You are welcome !"
+      if('want' in user_response  or 'know' in user_response or 'question'in user_response ):
+        k = "Yeah ! Tell me"
+      else:
+        if(greeting(user_response) != None):
+          k = "DOCBot: "+greeting(user_response)
+        else:
+          resp , typeq , variable , firstq ,action = response(user_response ,firstq ,typeq , variable)
+          k = "DOCBot: "+str(resp)
+    return k , action
+        
+
+
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("index.html")
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    
-    if request.method == 'POST':
-        user_input_question = clean(request.form.get('message'))
-        question0 = [[0]]
-        question0[0][0] = user_input_question
-        question = np.array(question0)
-        data = pd.DataFrame(question)
-        data["cleaned"] = question
- 
-        texts_test1 = data.cleaned.astype(str)
-        model1 = models.load_model('lstm.h5')
-        with open('dnntokenizer.pkl', 'rb') as f:
-            tokenizer = pickle.load(f)
- 
- 
-        sequences_test1 = tokenizer.texts_to_sequences(texts_test1)
-        x_test1 = pad_sequences(sequences_test1, maxlen=20)
-        pred31 = model1.predict(x_test1)
-        if (pred31 >0.5) == True : 
-            pred32 = "yes/no"
-        else :
-            pred32 = "open-ended"
+@app.route("/get")    
 
-        print('**************** QuestionType = ',pred32)
-        questtype = pred32
+def get_bot_response():
+    global action
+    userText = request.args.get('msg')
+    response,action = robot(userText)
+    return response
 
-        with open('pca.pkl', 'rb') as f :
-            pca = pickle.load(f)
-        with open('kmeans.pkl', 'rb') as f :
-            kmeans = pickle.load(f)
-        with open('tfidf.pkl', 'rb') as f :
-            tfidf = pickle.load(f)
- 
-        def cluster (userinput):
-             message = [[0]]
-             message[0][0] = userinput
-             message = np.array(message)
-             data = pd.DataFrame(message)
-             data["content"] = message
-             tf_idf = tfidf.transform(data["content"])
-             tf_idf_norm = normalize(tf_idf)
-             tf_idf_array = tf_idf_norm.toarray()
-             Y_sklearn = pca.transform(tf_idf_array)
-             prediction = kmeans.predict(Y_sklearn)
-             clusterid = str(prediction[0])
-             return clusterid
-        from googlesearch import search
-        def productq(inputq):
-             j = 'product not found'
-             query = "amazon "+inputq
-             try :
-                 for j in search(query, tld="com", num=1, stop=1):
-                     j = j.split(sep='.com/')[1]
-                     j = j.split(sep='/')[0]
-             except :
-                 j = 'product not found'
-             return j
-
-        with open('asindftfidfvector.pkl', 'rb') as f:
-            tfidf_vectorizer = pickle.load(f)
-        with open('asindftfidf.pkl', 'rb') as f:
-            tfidf_matrix = pickle.load(f)
- 
-        question= question0[0][0]
-        clusterid = cluster(question)
-        clusterresult = ' cluster'+clusterid
-        print("****************clusterresult",clusterresult)
-        
-
- 
-        query_vect = tfidf_vectorizer.transform([question])
-             
-        similarity = cosine_similarity(query_vect, tfidf_matrix)
-        top_5_simmi = similarity[0].argsort()[-5:][::-1]
- 
-        result = []
-        for i in top_5_simmi:
-            result.append(asindf.iloc[i]['asin'])
-        print('*************** Product code (asin)',result[0])
-        productcode = str(result[0])
-        productname = productq(productcode)
-        question = question + " " +str(result[0]) + " " +pred32 + " " +clusterresult+" "+productname
-        print("********************************* Final transformed question = ",question)
-        
-        with open('qtfidfvector.pkl', 'rb') as f:
-            tfidf_vectorizer = pickle.load(f)
-        with open('questiontfidf.pkl', 'rb') as f:
-            tfidf_matrix = pickle.load(f)
- 
-        query_vect = tfidf_vectorizer.transform([question])
-        similarity = cosine_similarity(query_vect, tfidf_matrix)
-        top_5_simmi = similarity[0].argsort()[-15:][::-1]
- 
-        result0 = []
-        result1 = []
-        result2 = []
- 
- 
-        for i in top_5_simmi:
-            result0.append(questiondf.iloc[i]['question'])
-            result1.append(answerdf.iloc[i]['answer'])       
-            result2.append(similarity[0, i])
- 
- 
-
-    return render_template('result.html', result0=result0,result1=result1,result2=result2,productcode=productcode,productname = productname,questtype=questtype,clusterid=clusterid)
- 
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
